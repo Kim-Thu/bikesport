e71114f91d25b945a5494387e9b08a5a0dc9b15d
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 23:20:16 +0700

    refactor(page): remove page-local inline form block

--- src/components/page/blocks/InlineFormBlock.tsx
+++
@@ -1,6 +0,0 @@
-import { InlineForm } from "@/components/form/InlineForm";
-import type { InlineFormBlockPayload } from "@/interfaces/page-block.interface";
-
-export function InlineFormBlock({ block }: { block: InlineFormBlockPayload }) {
-  return <InlineForm {...block.props} />;
-}
