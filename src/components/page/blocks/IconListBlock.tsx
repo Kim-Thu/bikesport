80a9e15e79554cd41cba48eebe49a59feaf0cc0a
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 23:20:09 +0700

    refactor(page): remove page-local icon list block

--- src/components/page/blocks/IconListBlock.tsx
+++
@@ -1,6 +0,0 @@
-import { IconList } from "@/components/list/IconList";
-import type { IconListBlockPayload } from "@/interfaces/page-block.interface";
-
-export function IconListBlock({ block }: { block: IconListBlockPayload }) {
-  return <IconList items={block.props.items} />;
-}
