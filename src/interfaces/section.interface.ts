876791fa704756830ddf65fc3fe9ceb3d145b6f3
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 03:40:08 +0700

    refactor(section): support section templates

--- src/interfaces/section.interface.ts
+++ src/interfaces/section.interface.ts
@@ -1,7 +1,13 @@
 import type { ElementType, ReactNode } from "react";
 
-export interface SectionProps {
+export type SectionTemplate = "default" | "flash-sale";
+
+export interface SectionTemplateProps {
   as?: ElementType;
   children: ReactNode;
   className?: string;
+}
+
+export interface SectionProps extends SectionTemplateProps {
+  template?: SectionTemplate;
 }
