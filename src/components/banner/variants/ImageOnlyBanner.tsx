00661b6f0afb74dc1ed072fa1d1d813db6170812
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 01:14:38 +0700

    fix(hero): increase image banner ratio height

--- src/components/banner/variants/ImageOnlyBanner.tsx
+++ src/components/banner/variants/ImageOnlyBanner.tsx
@@ -3,7 +3,7 @@ import type { BannerRecord } from "@/interfaces/banner.interface";
 
 export function ImageOnlyBanner({ banner }: { banner: BannerRecord }) {
   return (
-    <div className="relative aspect-hero-mobile w-full overflow-hidden rounded-xl border border-blue-100 bg-transparent sm:aspect-hero-tablet lg:aspect-hero">
+    <div className="relative aspect-hero-mobile w-full overflow-hidden rounded-xl border border-blue-100 bg-transparent sm:aspect-hero-tablet lg:aspect-8/3">
       <BannerBackground banner={banner} />
     </div>
   );
