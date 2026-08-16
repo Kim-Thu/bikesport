3c2d182e81194fa205519c4102029ac2890b3161
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 22:47:43 +0700

    refactor(pagebuilder): add icon list block

---
+++ src/components/page/blocks/IconListBlock.tsx
@@ -0,0 +1,6 @@
+import { IconList } from "@/components/list/IconList";
+import type { IconListBlockPayload } from "@/interfaces/page-block.interface";
+
+export function IconListBlock({ block }: { block: IconListBlockPayload }) {
+  return <IconList items={block.props.items} />;
+}
