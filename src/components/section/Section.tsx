65842ead88537b6e7889c5dcab3a8f06daaa59a3
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 15:31:35 +0700

    refactor(ui): merge Section Tailwind classes

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,5 +1,6 @@
 import type { SectionProps } from "@/interfaces/section.interface";
+import { cn } from "@/lib/classname.utils";
 
 export function Section({ as: Component = "section", children, className = "" }: SectionProps) {
-  return <Component className={className}>{children}</Component>;
+  return <Component className={cn(className)}>{children}</Component>;
 }
