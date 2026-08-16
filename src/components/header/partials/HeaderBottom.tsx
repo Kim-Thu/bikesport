957237ee392aaf70ec57bcc2c14dead5e60b9797
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 11:17:36 +0700

    refactor(header): support mobile bottom search layout

--- src/components/header/partials/HeaderBottom.tsx
+++ src/components/header/partials/HeaderBottom.tsx
@@ -11,9 +11,9 @@ export function HeaderBottom({ region }: HeaderPartialProps) {
     <div className="bg-white">
       <Container>
         {region.rows.map((row, rowIndex) => (
-          <Row key={row.id || rowIndex}>
+          <Row key={row.id || rowIndex} className="gap-2 max-lg:py-2 lg:gap-4">
             {row.columns?.map((column, columnIndex) => (
-              <Column key={column.id || columnIndex} grow={column.grow} className="flex items-center gap-4">
+              <Column key={column.id || columnIndex} grow={column.grow} className="flex min-w-0 items-center gap-2 lg:gap-4">
                 {column.items?.map(renderComponent)}
               </Column>
             ))}
