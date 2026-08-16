f77f0a763ef61ec24c0b29c843daee0e4ba7ed24
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 01:50:38 +0700

    feat(block): register media block

--- src/lib/page-block.registry.ts
+++ src/lib/page-block.registry.ts
@@ -2,6 +2,7 @@ import { CardBlock } from "@/components/block/CardBlock";
 import { CardGridBlock } from "@/components/block/CardGridBlock";
 import { IconListBlock } from "@/components/block/IconListBlock";
 import { InlineFormBlock } from "@/components/block/InlineFormBlock";
+import { MediaBlock } from "@/components/block/MediaBlock";
 import { MediaCtaBlock } from "@/components/block/MediaCtaBlock";
 import { ProductSliderBlock } from "@/components/block/ProductSliderBlock";
 import { TabsSliderBlock } from "@/components/block/TabsSliderBlock";
@@ -14,5 +15,6 @@ export const PAGE_BLOCK_COMPONENTS = {
   "card-grid": CardGridBlock,
   "icon-list": IconListBlock,
   "media-cta": MediaCtaBlock,
+  media: MediaBlock,
   "inline-form": InlineFormBlock,
 } satisfies Record<PageBlockComponent, unknown>;
