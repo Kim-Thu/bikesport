9754d2e07621ed6dc7606c204aa49631f259a4b5
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 23:20:25 +0700

    refactor(page): remove page-local media cta block

--- src/components/page/blocks/MediaCtaBlock.tsx
+++
@@ -1,6 +0,0 @@
-import { MediaCta } from "@/components/cta/MediaCta";
-import type { MediaCtaBlockPayload } from "@/interfaces/page-block.interface";
-
-export function MediaCtaBlock({ block }: { block: MediaCtaBlockPayload }) {
-  return <MediaCta {...block.props} />;
-}
