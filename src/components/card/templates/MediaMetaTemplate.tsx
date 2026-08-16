6fd8eddf695ca0120abe9f4172931c2d58a1be28
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 02:58:10 +0700

    style(card): use blue 200 for category label

--- src/components/card/templates/MediaMetaTemplate.tsx
+++ src/components/card/templates/MediaMetaTemplate.tsx
@@ -38,7 +38,7 @@ export function MediaMetaTemplate({
         {categoryName && categoryHref ? (
           <CLink
             href={categoryHref}
-            className="absolute left-4 top-4 rounded-md bg-white px-2 py-2 text-xs font-medium text-blue-600"
+            className="absolute left-4 top-4 rounded-md bg-blue-200 px-2 py-2 text-xs font-medium text-blue-700"
           >
             {categoryName}
           </CLink>
