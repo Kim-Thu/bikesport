2af6838c8dfa89ec41d62f5dfc28ac4dd456e1f3
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 01:52:48 +0700

    style(section): increase default vertical padding

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -2,5 +2,5 @@ import type { SectionProps } from "@/interfaces/section.interface";
 import { cn } from "@/lib/classname.utils";
 
 export function Section({ as: Component = "section", children, className = "" }: SectionProps) {
-  return <Component className={cn(className)}>{children}</Component>;
+  return <Component className={cn("py-6 sm:py-8", className)}>{children}</Component>;
 }
