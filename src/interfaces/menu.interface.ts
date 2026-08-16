bed11d34df3914a443e8f9c0c15895ae18a32a01
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 15:31:51 +0700

    refactor(menu): keep CMenu focused on menu items

--- src/interfaces/menu.interface.ts
+++ src/interfaces/menu.interface.ts
@@ -1,11 +1,5 @@
-import type { HeadingLevel } from "@/interfaces/heading.interface";
-
 export interface CMenuProps {
   menuId: string;
-  title?: string;
-  headingLevel?: HeadingLevel;
-  sectionClassName?: string;
-  headingClassName?: string;
   listClassName?: string;
   itemClassName?: string;
   linkClassName?: string;
