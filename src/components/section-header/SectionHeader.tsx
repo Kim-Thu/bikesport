0d977d4b094cf8cd9ce96406e0af6e26c43128fc
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 03:35:47 +0700

    refactor(section-header): register flash sale template

--- src/components/section-header/SectionHeader.tsx
+++ src/components/section-header/SectionHeader.tsx
@@ -1,8 +1,10 @@
 import { DefaultTemplate } from "@/components/section-header/templates/DefaultTemplate";
-import type { SectionHeaderProps } from "@/interfaces/section-header.interface";
+import { FlashSaleTemplate } from "@/components/section-header/templates/FlashSaleTemplate";
+import type { SectionHeaderProps, SectionHeaderTemplate } from "@/interfaces/section-header.interface";
 
-const SECTION_HEADER_TEMPLATES = {
+const SECTION_HEADER_TEMPLATES: Record<SectionHeaderTemplate, (props: SectionHeaderProps) => React.ReactNode> = {
   default: DefaultTemplate,
+  "flash-sale": FlashSaleTemplate,
 };
 
 export function SectionHeader({ template = "default", ...props }: SectionHeaderProps) {
