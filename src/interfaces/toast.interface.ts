5b3c1702bd2cc5fe3be871364ddf6823bdd23e36
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 12:49:44 +0700

    feat(ui): add toast interfaces

---
+++ src/interfaces/toast.interface.ts
@@ -0,0 +1,14 @@
+export type ToastType = "success" | "error" | "info" | "warning";
+
+export interface ToastItem {
+  id: string;
+  message: string;
+  type: ToastType;
+  duration?: number;
+}
+
+export interface AddToastInput {
+  message: string;
+  type?: ToastType;
+  duration?: number;
+}
