d0459b0c3876413855bc9690ec07dbc6e3634d79
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 23:18:13 +0700

    refactor(section): move content aside section out of page

---
+++ src/components/section/ContentAsideSection.tsx
@@ -0,0 +1,26 @@
+import { BlockRenderer } from "@/components/block/BlockRenderer";
+import { Container } from "@/components/layout/Container";
+import { Section } from "@/components/section/Section";
+import type { ContentAsideSectionPayload } from "@/interfaces/page.interface";
+import { cn } from "@/lib/classname.utils";
+
+export function ContentAsideSection({ section }: { section: ContentAsideSectionPayload }) {
+  return (
+    <Section className={section.props.sectionClassName}>
+      <Container>
+        <div className={cn("grid gap-6 lg:grid-cols-[minmax(0,3fr)_minmax(16rem,1fr)]", section.props.gridClassName)}>
+          <div className={cn("min-w-0 space-y-6", section.props.contentClassName)}>
+            {section.content.map((block) => (
+              <BlockRenderer key={block._id} block={block} />
+            ))}
+          </div>
+          <aside className={cn("space-y-4", section.props.asideClassName)}>
+            {section.aside.map((block) => (
+              <BlockRenderer key={block._id} block={block} />
+            ))}
+          </aside>
+        </div>
+      </Container>
+    </Section>
+  );
+}
