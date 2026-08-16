3a0f3c772cd2b23ee75b8572db82038ef7780a35
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 14:40:40 +0700

    feat(footer): render footer on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { Footer } from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
-import type { HeaderSettings } from "@/interfaces/header.interface";
 import wpOption from "@/data/wp-option.json";
+import type { FooterSettings } from "@/interfaces/footer.interface";
+import type { HeaderSettings } from "@/interfaces/header.interface";
 
 export default function Home() {
   return (
@@ -9,6 +11,7 @@ export default function Home() {
       <main aria-label="Nội dung trang chủ">
         <div className="mx-auto my-6 min-h-screen max-w-screen-2xl rounded-xl bg-gradient-to-br from-blue-50 to-white px-6" />
       </main>
+      <Footer settings={wpOption.footer as FooterSettings} />
     </>
   );
 }
