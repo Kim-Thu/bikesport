c3d60549bcad03cb890c7e7f7219a655c6da9ae1
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-16 15:47:23 +0700

    refactor(footer): remove payment asset config from footer settings

--- src/interfaces/footer.interface.ts
+++ src/interfaces/footer.interface.ts
@@ -1,20 +1,10 @@
 import type { FooterTemplateName } from "@/types/footer.type";
 
-export interface FooterAsset {
-  label: string;
-  src: string;
-  alt: string;
-  width: number;
-  height: number;
-  enabled?: boolean;
-}
-
 export interface FooterSettings {
   name: string;
   template: FooterTemplateName;
   description?: string;
   menuIds?: string[];
-  assets?: FooterAsset[];
   copyright?: string;
 }
 
