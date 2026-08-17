66b3a009c797541af4959db2cc8395af8e2cd1bf
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 14:47:23 +0700

    refactor(data): wire json promotion data source

--- src/data-access/data-sources.ts
+++ src/data-access/data-sources.ts
@@ -4,6 +4,7 @@ import { jsonBrandDataSource } from "@/data-access/json/json-brand-data-source";
 import { jsonCategoryDataSource } from "@/data-access/json/json-category-data-source";
 import { jsonPageDataSource } from "@/data-access/json/json-page-data-source";
 import { jsonProductDataSource } from "@/data-access/json/json-product-data-source";
+import { jsonPromotionDataSource } from "@/data-access/json/json-promotion-data-source";
 import { mongodbDataSources } from "@/data-access/mongodb/mongodb-data-sources";
 
 const jsonDataSources: DataSources = {
@@ -11,6 +12,7 @@ const jsonDataSources: DataSources = {
   category: jsonCategoryDataSource,
   brand: jsonBrandDataSource,
   product: jsonProductDataSource,
+  promotion: jsonPromotionDataSource,
 };
 
 function createDataSources(): DataSources {
