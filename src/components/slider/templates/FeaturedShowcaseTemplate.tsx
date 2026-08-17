35072c7da8859bbf526f2e80021e66c06cc88d5b
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 10:59:33 +0700

    refactor: pass showcase action tone

--- src/components/slider/templates/FeaturedShowcaseTemplate.tsx
+++ src/components/slider/templates/FeaturedShowcaseTemplate.tsx
@@ -6,6 +6,7 @@ export function FeaturedShowcaseTemplate({
   slider,
   href,
   actionLabel = "Xem tất cả",
+  actionTone,
   backgroundMediaId,
   containerClassName,
 }: TabsSliderTemplateProps) {
@@ -15,6 +16,7 @@ export function FeaturedShowcaseTemplate({
       content={slider}
       href={href}
       actionLabel={actionLabel}
+      actionTone={actionTone}
       backgroundMediaId={backgroundMediaId}
       containerClassName={containerClassName}
     />
