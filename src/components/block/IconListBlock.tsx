b45e8e2590a950b4839e8fbfea06c9d32c49bd40
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 12:34:17 +0700

    refactor: pass icon list layout

--- src/components/block/IconListBlock.tsx
+++ src/components/block/IconListBlock.tsx
@@ -2,5 +2,5 @@ import { IconList } from "@/components/list/IconList";
 import type { IconListBlockPayload } from "@/interfaces/page-block.interface";
 
 export function IconListBlock({ block }: { block: IconListBlockPayload }) {
-  return <IconList items={block.props.items} />;
+  return <IconList items={block.props.items} layout={block.props.layout} />;
 }
