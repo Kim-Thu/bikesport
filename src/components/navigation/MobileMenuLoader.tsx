293ce25a4395f15e1defd99ff959b49d654e4bc2
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 04:29:17 +0700

    refactor(media): resolve mobile logo media on server

--- src/components/navigation/MobileMenuLoader.tsx
+++ src/components/navigation/MobileMenuLoader.tsx
@@ -1,5 +1,6 @@
 import { MobileMenu } from "@/components/navigation/MobileMenu";
 import type { NavMenuProps } from "@/interfaces/navigation.interface";
+import { getMediaById } from "@/lib/media.utils";
 import { getMenuById } from "@/lib/menu-data.utils";
 import { getSiteOptions } from "@/lib/options.utils";
 
@@ -7,10 +8,15 @@ export async function MobileMenuLoader({ menuId }: NavMenuProps) {
   const [menu, options] = await Promise.all([getMenuById(menuId), getSiteOptions()]);
   if (!menu || !options) return null;
 
+  const logoMedia = options.site.logoMediaId
+    ? await getMediaById(options.site.logoMediaId)
+    : null;
+
   return (
     <MobileMenu
       menu={menu}
       site={options.site}
+      logoMedia={logoMedia}
       hotline={options.contact.hotline}
       socialItems={options.contact.social ?? []}
     />
