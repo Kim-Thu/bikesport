22bd0ab5a0e343c9652aaa3b4ed31c1b434271ef
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 10:20:30 +0700

    feat: add product slider layout template props

---
+++ src/interfaces/product-slider-layout.interface.ts
@@ -0,0 +1,8 @@
+import type { ReactNode } from "react";
+
+export interface ProductSliderLayoutTemplateProps {
+  header?: ReactNode;
+  slider: ReactNode;
+  href?: string;
+  actionLabel?: string;
+}
