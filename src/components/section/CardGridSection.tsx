9d023d707b34f498325e9bf7a1a8ac2de8872c31
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 11:46:52 +0700

    refactor: show empty content for category sections

--- src/components/section/CardGridSection.tsx
+++ src/components/section/CardGridSection.tsx
@@ -1,4 +1,5 @@
 import { Card } from "@/components/card/Card";
+import { EmptyContent } from "@/components/empty-content/EmptyContent";
 import { Container } from "@/components/layout/Container";
 import { Section } from "@/components/section/Section";
 import { SectionHeader } from "@/components/section-header/SectionHeader";
@@ -8,7 +9,6 @@ import { cn } from "@/lib/classname.utils";
 
 export function CardGridSection({ section }: { section: CardGridSectionPayload }) {
   const items = getFeaturedCategoriesByType(section.props.source.categoryType, section.props.source.limit);
-  if (!items.length) return null;
 
   return (
     <Section className={section.props.sectionClassName}>
@@ -22,17 +22,22 @@ export function CardGridSection({ section }: { section: CardGridSectionPayload }
           template={section.props.headingTemplate}
           className="mb-4"
         />
-        <div className={cn("grid grid-cols-2 gap-3 sm:grid-cols-3 lg:grid-cols-6", section.props.gridClassName)}>
-          {items.map((item) => (
-            <Card
-              key={item._id}
-              template={section.props.template}
-              title={item.name}
-              href={`/danh-muc/${item.slug}`}
-              mediaId={item.mediaId}
-            />
-          ))}
-        </div>
+
+        {items.length ? (
+          <div className={cn("grid grid-cols-2 gap-3 sm:grid-cols-3 lg:grid-cols-6", section.props.gridClassName)}>
+            {items.map((item) => (
+              <Card
+                key={item._id}
+                template={section.props.template}
+                title={item.name}
+                href={`/danh-muc/${item.slug}`}
+                mediaId={item.mediaId}
+              />
+            ))}
+          </div>
+        ) : (
+          <EmptyContent />
+        )}
       </Container>
     </Section>
   );
