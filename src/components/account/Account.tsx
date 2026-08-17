33b8875c9250ea54fba8f3c8b88ea7ec488c2f97
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 03:12:50 +0700

    refactor(options): decouple account from raw options

--- src/components/account/Account.tsx
+++ src/components/account/Account.tsx
@@ -1,9 +1,9 @@
 import { Icon } from "@/components/icon/Icon";
 import { CLink } from "@/components/link/CLink";
-import wpOption from "@/data/wp-option.json";
+import type { NavigationOption } from "@/interfaces/options.interface";
 
-export function Account() {
-  const { label, href } = wpOption.account;
+export function Account({ account }: { account: NavigationOption }) {
+  const { label, href } = account;
 
   return (
     <CLink href={href} className="flex min-w-8 flex-col items-center gap-1 whitespace-nowrap text-xs font-semibold lg:min-w-16">
