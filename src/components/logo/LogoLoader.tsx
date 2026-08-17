e4a39339b6204ebaf8150c7397a9f920ceed54c6
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 04:28:58 +0700

    refactor(media): resolve logo media in server loader

--- src/components/logo/LogoLoader.tsx
+++ src/components/logo/LogoLoader.tsx
@@ -1,10 +1,15 @@
 import { Logo } from "@/components/logo/Logo";
 import type { LogoProps } from "@/interfaces/logo.interface";
+import { getMediaById } from "@/lib/media.utils";
 import { getSiteOptions } from "@/lib/options.utils";
 
-type LogoLoaderProps = Omit<LogoProps, "site">;
+type LogoLoaderProps = Omit<LogoProps, "site" | "logoMedia">;
 
 export async function LogoLoader(props: LogoLoaderProps) {
   const options = await getSiteOptions();
-  return <Logo {...props} site={options.site} />;
+  const logoMedia = options.site.logoMediaId
+    ? await getMediaById(options.site.logoMediaId)
+    : null;
+
+  return <Logo {...props} site={options.site} logoMedia={logoMedia} />;
 }
