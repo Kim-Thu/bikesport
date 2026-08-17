c39eafb5e99121333a203cb9e1fbeb37c6a9c642
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 15:05:13 +0700

    refactor(data): register content data sources

--- src/data-access/contracts/data-sources.interface.ts
+++ src/data-access/contracts/data-sources.interface.ts
@@ -1,9 +1,12 @@
 import type { BrandDataSource } from "@/data-access/contracts/brand-data-source.interface";
 import type { CategoryDataSource } from "@/data-access/contracts/category-data-source.interface";
+import type { EventDataSource } from "@/data-access/contracts/event-data-source.interface";
 import type { MenuDataSource } from "@/data-access/contracts/menu-data-source.interface";
 import type { PageDataSource } from "@/data-access/contracts/page-data-source.interface";
+import type { PostDataSource } from "@/data-access/contracts/post-data-source.interface";
 import type { ProductDataSource } from "@/data-access/contracts/product-data-source.interface";
 import type { PromotionDataSource } from "@/data-access/contracts/promotion-data-source.interface";
+import type { UserDataSource } from "@/data-access/contracts/user-data-source.interface";
 
 export interface DataSources {
   page: PageDataSource;
@@ -12,4 +15,7 @@ export interface DataSources {
   product: ProductDataSource;
   promotion: PromotionDataSource;
   menu: MenuDataSource;
+  post: PostDataSource;
+  event: EventDataSource;
+  user: UserDataSource;
 }
