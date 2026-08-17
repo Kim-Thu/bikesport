1e164ac3e0c1e4daa73605c3845d68b670dc38ac
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 10:38:44 +0700

    refactor: remove redundant product slider source wrapper

--- src/lib/product-slider-source.utils.ts
+++
@@ -1,7 +0,0 @@
-import type { ProductCollectionItem } from "@/interfaces/product-collection-item.interface";
-import type { ProductSource } from "@/interfaces/product-source.interface";
-import { getProductCollectionItems } from "@/lib/product-collection-source.utils";
-
-export function getProductSliderItems(source: ProductSource): ProductCollectionItem[] {
-  return getProductCollectionItems(source);
-}
