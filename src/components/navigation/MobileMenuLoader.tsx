c8d49eb9abc4beef007d0e61b5784569730068ab
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 04:33:35 +0700

    refactor(media): batch resolve mobile menu media

--- src/components/navigation/MobileMenuLoader.tsx
+++ src/components/navigation/MobileMenuLoader.tsx
@@ -1,6 +1,6 @@
 import { MobileMenu } from "@/components/navigation/MobileMenu";
 import type { NavMenuProps } from "@/interfaces/navigation.interface";
-import { getMediaById } from "@/lib/media.utils";
+import { getMediaById, getMediaWithFallbackByIds } from "@/lib/media.utils";
 import { getMenuById } from "@/lib/menu-data.utils";
 import { getSiteOptions } from "@/lib/options.utils";
 
@@ -8,15 +8,18 @@ export async function MobileMenuLoader({ menuId }: NavMenuProps) {
   const [menu, options] = await Promise.all([getMenuById(menuId), getSiteOptions()]);
   if (!menu || !options) return null;
 
-  const logoMedia = options.site.logoMediaId
-    ? await getMediaById(options.site.logoMediaId)
-    : null;
+  const menuMediaIds = menu.items.flatMap((item) => (item.mediaId ? [item.mediaId] : []));
+  const [logoMedia, menuMediaById] = await Promise.all([
+    options.site.logoMediaId ? getMediaById(options.site.logoMediaId) : Promise.resolve(null),
+    getMediaWithFallbackByIds(menuMediaIds),
+  ]);
 
   return (
     <MobileMenu
       menu={menu}
       site={options.site}
       logoMedia={logoMedia}
+      menuMediaById={menuMediaById}
       hotline={options.contact.hotline}
       socialItems={options.contact.social ?? []}
     />
