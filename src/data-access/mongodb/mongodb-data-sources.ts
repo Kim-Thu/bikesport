d44b993e43a61350cc1c8b0be887479e19c87f62
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 06:33:22 +0700

    refactor(review): register mongodb review data source

--- src/data-access/mongodb/mongodb-data-sources.ts
+++ src/data-access/mongodb/mongodb-data-sources.ts
@@ -17,6 +17,7 @@ import { createMongoPaymentDataSource } from "@/data-access/mongodb/mongodb-paym
 import { createMongoPostDataSource } from "@/data-access/mongodb/mongodb-post-data-source";
 import { createMongoProductDataSource } from "@/data-access/mongodb/mongodb-product-data-source";
 import { createMongoPromotionDataSource } from "@/data-access/mongodb/mongodb-promotion-data-source";
+import { createMongoReviewDataSource } from "@/data-access/mongodb/mongodb-review-data-source";
 import { createMongoStoreDataSource } from "@/data-access/mongodb/mongodb-store-data-source";
 import { createMongoUserDataSource } from "@/data-access/mongodb/mongodb-user-data-source";
 import type { MongoDatabaseProvider } from "@/data-access/mongodb/mongodb-driver.interface";
@@ -44,6 +45,7 @@ export function createMongoDataSources(
     options: createMongoOptionsDataSource(getDatabase),
     media: createMongoMediaDataSource(getDatabase),
     order: createMongoOrderDataSource(getDatabase),
+    review: createMongoReviewDataSource(getDatabase),
   };
 }
 
