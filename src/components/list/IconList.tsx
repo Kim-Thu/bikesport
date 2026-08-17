d010df472da545f78040e1bea1f148e30513d59b
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 13:00:09 +0700

    style: align icon list text sizes

--- src/components/list/IconList.tsx
+++ src/components/list/IconList.tsx
@@ -21,7 +21,7 @@ function IconListItemContent({ item }: { item: IconListItem }) {
       title={item.title}
       description={item.description}
       iconClassName="h-8 w-8 text-blue-600"
-      titleClassName="text-base normal-case text-gray-900"
+      titleClassName="text-sm normal-case text-gray-900"
       descriptionClassName="text-sm text-gray-500 opacity-100"
     />
   );
