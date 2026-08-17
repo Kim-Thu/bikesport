a6cb1e79c143ff9ca5e54fbb6066be5ad61d9159
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-17 14:57:53 +0700

    refactor(data): register menu data source

--- src/data-access/contracts/data-sources.interface.ts
+++ src/data-access/contracts/data-sources.interface.ts
@@ -1,5 +1,6 @@
 import type { BrandDataSource } from "@/data-access/contracts/brand-data-source.interface";
 import type { CategoryDataSource } from "@/data-access/contracts/category-data-source.interface";
+import type { MenuDataSource } from "@/data-access/contracts/menu-data-source.interface";
 import type { PageDataSource } from "@/data-access/contracts/page-data-source.interface";
 import type { ProductDataSource } from "@/data-access/contracts/product-data-source.interface";
 import type { PromotionDataSource } from "@/data-access/contracts/promotion-data-source.interface";
@@ -10,4 +11,5 @@ export interface DataSources {
   brand: BrandDataSource;
   product: ProductDataSource;
   promotion: PromotionDataSource;
+  menu: MenuDataSource;
 }
