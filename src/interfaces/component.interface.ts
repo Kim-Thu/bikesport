eee785789637bad8f69bdfa65ca57a407ae538d1
Kim-Thu <49784235+Kim-Thu@users.noreply.github.com>
2026-08-18 08:03:06 +0700

    fix(config): require component props when component requires them

--- src/interfaces/component.interface.ts
+++ src/interfaces/component.interface.ts
@@ -1,9 +1,14 @@
 import type { ComponentName, ComponentPropsMap } from "@/types/component.type";
 
+type ComponentItemBase<Name extends ComponentName> = {
+  component: Name;
+  enabled?: boolean;
+};
+
+type ComponentItemFor<Name extends ComponentName> = {} extends ComponentPropsMap[Name]
+  ? ComponentItemBase<Name> & { props?: ComponentPropsMap[Name] }
+  : ComponentItemBase<Name> & { props: ComponentPropsMap[Name] };
+
 export type ComponentItem = {
-  [Name in ComponentName]: {
-    component: Name;
-    enabled?: boolean;
-    props?: ComponentPropsMap[Name];
-  };
+  [Name in ComponentName]: ComponentItemFor<Name>;
 }[ComponentName];
